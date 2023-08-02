d9bcce015c2020ce1ec859d583f866c6fb9dadb9
Khushi Roy <roykhushi432@gmail.com>
2023-08-02 17:26:30 +0530

    Create FindSqRoot.java

---
+++ FindSqRoot.java
@@ -0,0 +1,30 @@
+//Find the square root of a given non integer number rounded off to nearest floor integer value.
+
+public class FindSqRoot {
+
+    public static int sqrt(int x){
+        int start = 0;
+        int end = x;
+        int ans = -1;
+        while(start <= end){
+            int mid = start+(end-start)/2;
+            long value = mid*mid; //long--> to avoid overflow
+
+            if(value == x){ //perfect squares
+                return mid;
+            }
+            else if(value > x){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+                ans = mid; //for non perfect squares
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int x = 29;
+        System.out.println(sqrt(x));
+    }
+}
