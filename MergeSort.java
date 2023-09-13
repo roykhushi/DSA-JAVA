8bdd54f4c450733a42c9ba585ec7262407d95448
Khushi Roy <roykhushi432@gmail.com>
2023-09-13 16:13:22 +0530

    merge sort again

--- MergeSort.java
+++ MergeSort.java
@@ -7,25 +7,26 @@ public class MergeSort {
 
     public static void conquer(int arr [], int startIndx, int mid, int endIndex){
         int merged [] = new int [endIndex - startIndx +1];
-        int idx1 = startIndx; //traversing the first subarray
-        int idx2 = mid+1; // traversing the second subarray
+        int idx1 = startIndx; //traversing the left subarray
+        int idx2 = mid+1; // traversing the right subarray
         int x = 0; // traversing the merged array
 
         while(idx1 <= mid && idx2 <= endIndex){
-            if(arr[idx1] <= arr[idx2]){
-                merged[x++] = arr[idx1++];
+            if(arr[idx1] <= arr[idx2]){ // comparing the elements in two subarrays
+                merged[x++] = arr[idx1++]; //feeding, if any value is smaller in the left subarray into the new array and increasing the pointer to search for the next elements
             }
             else{
-                merged[x++] = arr[idx2++];
+                merged[x++] = arr[idx2++];  // if right subarray has the smaller value
             }
         } 
+        //feeding the left out values from the two subarrays into the new array 
         while(idx1 <= mid){
-            merged[x++] = arr[idx1++];
+            merged[x++] = arr[idx1++]; 
         }
         while(idx2 <= endIndex){
             merged[x++] = arr[idx2++];
         }
-        for(int i=0,j=startIndx; i<merged.length;i++,j++){
+        for(int i=0,j=startIndx; i<merged.length;i++,j++){ //merging the array
             arr[j] = merged[i];
         }
 
@@ -35,10 +36,10 @@ public class MergeSort {
         if(startIndx >= endIndex){
             return;
         }
-        int mid= startIndx + (endIndex-startIndx)/2;
-        divide(arr, startIndx, mid);
-        divide(arr, mid+1, endIndex);
-        conquer(arr, startIndx, mid, endIndex);
+        int mid = startIndx + (endIndex-startIndx)/2; //for diving the array into two halves
+        divide(arr, startIndx, mid); //left subarray
+        divide(arr, mid+1, endIndex); //right subarray
+        conquer(arr, startIndx, mid, endIndex); //merging 
     }
 
     public static void main(String[] args) {
