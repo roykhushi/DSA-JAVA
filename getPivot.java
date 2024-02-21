3f955822310b71547b001631778275e16b9bb18a
Khushi Roy <roykhushi432@gmail.com>
2024-02-21 13:30:38 +0530

    edit

--- getPivot.java
+++ getPivot.java
@@ -5,7 +5,7 @@ public class getPivot {
         int start = 0;
         int end = n-1;
         while (start < end) {
-            int mid = start + (end=start)/2;
+            int mid = start + (end-start)/2;
             if(arr[mid] >= arr[0]){//bs on first line
                 start = mid+1;
             }
